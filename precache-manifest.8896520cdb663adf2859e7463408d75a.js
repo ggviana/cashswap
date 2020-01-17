f2b1d56fe26cc7f1f9113645e1a91db26bc30e92
Guilherme Viana <ggviana@hotmail.com.br>
2020-01-17 00:09:29 -0300

    Updates

---
+++ precache-manifest.8896520cdb663adf2859e7463408d75a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa3820a55e723ea0097217b6ad59ed8",
+    "url": "/cashswap/index.html"
+  },
+  {
+    "revision": "66375bd01ef967331df7",
+    "url": "/cashswap/static/css/2.8a22d9f7.chunk.css"
+  },
+  {
+    "revision": "66375bd01ef967331df7",
+    "url": "/cashswap/static/js/2.58f9cf59.chunk.js"
+  },
+  {
+    "revision": "e9260139e7fb258a0a66563335ba0615",
+    "url": "/cashswap/static/js/2.58f9cf59.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c89d310092a643531f7f",
+    "url": "/cashswap/static/js/main.8b9b1316.chunk.js"
+  },
+  {
+    "revision": "e2bdb714b671a6dd4856",
+    "url": "/cashswap/static/js/runtime-main.3da114c7.js"
+  },
+  {
+    "revision": "cc8307fe4e1ea470ff6ab837ec06992a",
+    "url": "/cashswap/static/media/logo.cc8307fe.svg"
+  }
+]);
