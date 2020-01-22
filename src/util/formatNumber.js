b8f11291dbefb2ce4983730d8e2f87feefc98f68
Guilherme Viana <ggviana@hotmail.com.br>
2020-01-22 17:27:47 -0300

    Fixed rounding error

--- src/util/formatNumber.js
+++ src/util/formatNumber.js
@@ -1,5 +1,11 @@
 import numeral from 'numeral'
 
 export default function formatNumber (value) {
-  return numeral(value).format('0.00[00]')
+  return numeral(value).format('0.00', raw => {
+    const value = numeral(raw).value()
+    const integer = parseInt(value)
+    const cents = Math.floor(value - integer)
+
+    return integer + cents
+  })
 }
