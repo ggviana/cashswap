fe8e0d30d87e4fdbabdb8261044e384e32e42751
Guilherme Viana <ggviana@hotmail.com.br>
2020-01-30 16:19:35 -0300

    Fixed pooling

--- src/hooks/useExchangeRate.js
+++ src/hooks/useExchangeRate.js
@@ -1,12 +1,16 @@
 import { useEffect, useState } from 'react'
 import BalanceService from '../services/Balance'
 
-export default function useExchangeRate (from, to) {
+export default function useExchangeRate (from, to, timeout = 5000) {
   const [rate, setRate] = useState(null)
 
   useEffect(() => {
-    BalanceService.getRate({ from, to }).then(setRate)
-  }, [from, to])
+    const fetchExchangeRate = () => BalanceService.getRate({ from, to }).then(setRate)
+    fetchExchangeRate()
+
+    const interval = window.setInterval(fetchExchangeRate, timeout)
+    return () => window.clearInterval(interval)
+  }, [from, to, timeout])
 
   return rate
 }
